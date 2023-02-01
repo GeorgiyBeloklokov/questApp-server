3d00b4a8346a1eb099ab610c06ee41000bcd66bb
GeorgiyBeloklokov <georgiy.reus@gmail.com>
2023-02-02 00:04:52 +0300

    feat: login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,11 @@
+import { User } from 'src/users/users.model';
 import { UsersService } from './../users/users.service';
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
@@ -11,7 +17,8 @@ export class AuthService {
   ) {}
 
   async login(userDto: CreateUserDto) {
-    return null;
+    const user = await this.validateUser(userDto);
+    return this.generateToken(user);
   }
 
   async registration(userDto: CreateUserDto) {
@@ -29,12 +36,24 @@ export class AuthService {
     });
     return this.generateToken(user);
   }
-  async generateToken(user) {
+  private async generateToken(user: User) {
     const payload = {
       email: user.email,
       id: user.id,
       roles: user.roles,
     };
     return { token: this.jwtService.sign(payload) };
+  }
+
+  private async validateUser(userDto: CreateUserDto) {
+    const user = await this.userService.getUsersByEmail(userDto.email);
+    const passwordEquals = await bcrypt.compare(
+      userDto.password,
+      user.password,
+    );
+    if (user && passwordEquals) {
+      return user;
+    }
+    throw new UnauthorizedException({ message: 'Wrong email or password' });
   }
 }
