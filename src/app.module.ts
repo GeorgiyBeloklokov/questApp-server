54ac4872e55d1f48585c13f0400cf27fede057c6
GeorgiyBeloklokov <georgiy.reus@gmail.com>
2023-02-01 22:36:37 +0300

    feat: roles

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { User } from './users/users.model';
+import { RoleModule } from './role/role.module';
+import { Role } from './role/role.model';
+import { UserRoles } from './role/user-roles.model';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   controllers: [],
@@ -16,10 +20,12 @@ import { User } from './users/users.model';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [User],
+      models: [User, Role, UserRoles],
       autoLoadModels: true,
     }),
     UsersModule,
+    RoleModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,13 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { Role } from 'src/role/role.model';
+import { UserRoles } from 'src/role/user-roles.model';
+import { RoleModule } from 'src/role/role.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [SequelizeModule.forFeature([User, Role, UserRoles]), RoleModule],
 })
 export class UsersModule {}
