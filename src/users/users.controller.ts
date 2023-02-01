58ffb1e5d137760ab8d92ede783f665a82759a3a
GeorgiyBeloklokov <georgiy.reus@gmail.com>
2023-02-01 19:53:54 +0300

    feat:env modes, getUsers, createUser

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  create(@Body() userDto: CreateUserDto) {
+    return this.usersService.createUsers(userDto);
+  }
+  @Get()
+  getAll() {
+    return this.usersService.getAllUsers();
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,3 +1,5 @@
+import { User } from './users.model';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -5,5 +7,6 @@ import { UsersService } from './users.service';
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  imports: [SequelizeModule.forFeature([User])],
 })
 export class UsersModule {}
