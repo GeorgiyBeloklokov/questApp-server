ae74e1dbe564af2c759d17fa66bff0504ffd330d
GeorgiyBeloklokov <georgiy.reus@gmail.com>
2023-02-02 22:28:46 +0300

    fix: deploy

---
+++ dist/users/users.controller.d.ts
@@ -0,0 +1,13 @@
+import { BanUserDto } from './dto/ban-user.dto';
+import { AddRoleDto } from './dto/add-role.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './users.model';
+export declare class UsersController {
+    private usersService;
+    constructor(usersService: UsersService);
+    create(userDto: CreateUserDto): Promise<User>;
+    getAll(): Promise<User[]>;
+    addRole(dto: AddRoleDto): Promise<AddRoleDto>;
+    ban(dto: BanUserDto): Promise<User>;
+}
