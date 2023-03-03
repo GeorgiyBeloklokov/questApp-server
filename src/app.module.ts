806209961b07328e29fb6935dcc482f217331eb5
GeorgiyBeloklokov <georgiy.reus@gmail.com>
2023-03-03 15:22:23 +0300

    fix: deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { Answer } from './answer/answer.model';
   controllers: [],
   providers: [],
   imports: [
-    ConfigModule.forRoot({ envFilePath: `.${process.env.NODE_ENV}` }),
+    ConfigModule.forRoot({ envFilePath: '.env' }),
     ServeStaticModule.forRoot({
       rootPath: path.resolve(__dirname, 'static'),
     }),
